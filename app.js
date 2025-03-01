1fdc3717c01760c8cf2c9f0c49dea2ea2c3b4c46
ranajitchandra <ranajitchandradas3@gmail.com>
2025-03-01 23:30:17 +0600

    fix update time capture

--- app.js
+++ app.js
@@ -20,16 +20,19 @@ document.getElementById("day-name").innerText = day
 document.getElementById("date-view").innerText = `${date}-${month}-${year}`
 
 
-
 let btn = document.querySelectorAll("#btn")
-let doneTaskList =parseInt(document.getElementById("done-task-list").innerText)
-let con = 
-    console.log(doneTaskList.innerText = doneTaskList.innerText +1)
 let totalTask = document.getElementById("total-task")
     totalTask.innerText = btn.length
-    // console.log(totalTask.innerText = -1)
+
 for(i=0; i<btn.length; i++){
     btn[i].addEventListener("click", function(e){
+        const currentDate = new Date();
+        let hour = currentDate.getHours().toString()
+            hour = hour % 12
+        let minute = currentDate.getMinutes().toString()
+        let second = currentDate.getSeconds().toString()
+        let amPM = hour >= 12 ? "PM" : "AM";
+        let time = `${hour}:${minute}:${second} ${amPM}`
         e.target.classList.add("cursor-not-allowed", "bg-gray-200")
         e.target.classList.remove("hover:bg-blue-700")
         //_______________________________________
@@ -43,15 +46,20 @@ for(i=0; i<btn.length; i++){
         for(j=0; j<e.target.classList.length; j++){
             // console.log( e.target.classList[j])
             if(e.target.classList[j] === "bg-gray-200"){
-                totalTask.innerText =totalTask.innerText - 1
+                totalTask.innerText = totalTask.innerText - 1
+                let = doneTaskList = parseInt(document.getElementById("done-task-list").innerText);
+                document.getElementById("done-task-list").innerText = doneTaskList + 1
+                let parentBox = document.getElementById("parent-box").innerHTML += `
+                <p class="bg-blue-50 p-3 my-2 rounded-md">You have completed the task <span class="font-medium">${h2Title}</span> at <br> ${time}
+                </p>
+                `;
                 
+
             }
         }
         
     })
 }
-
-
 
 
 
