9474566fc51ca2d9bed554f71e552d19337bb97f
ranajitchandra <ranajitchandradas3@gmail.com>
2025-03-01 08:31:51 +0600

    disabled

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ document.getElementById("date-view").innerText = `${date}-${month}-${year}`
 
 
 
+let btn = document.querySelectorAll("#btn")
+let totalTask = document.getElementById("total-task")
+    totalTask.innerText = btn.length
+    // console.log(totalTask.innerText = -1)
+for(i=0; i<btn.length; i++){
+    btn[i].addEventListener("click", function(e){
+        e.target.classList.add("cursor-not-allowed", "bg-gray-200")
+        e.target.classList.remove("hover:bg-blue-700")
+        // console.log(e.target.parentNode.parentNode.childNodes[1].nextElementSibling.innerText)
+        let h2Title = e.target.parentNode.parentNode.querySelector("h2").innerText
+        // console.log( e.target.classList.length)
+        for(j=0; j<e.target.classList.length; j++){
+            // console.log(e.target.classList[j] === "bg-gray-200")
+            if(e.target.classList[j] === "bg-gray-200"){
+                totalTask.innerText =totalTask.innerText - 1
+            }
+        }
+        
+    })
+}
 
 
 
